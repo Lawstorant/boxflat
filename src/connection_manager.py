c72dc98e96fa26a92e24e9abbc29b156bc3d70c7
Tomasz Pakuła <forest10pl@gmail.com>
2024-07-06 23:06:02 +0200

    Working serial connection

--- src/connection_manager.py
+++ src/connection_manager.py
@@ -12,62 +12,55 @@ with open("serial.yml") as stream:
         print(exc)
         quit(1)
 
-    MAGIC_VALUE = SERIAL_VALUES["magic-value"]
-    MESSAGE_START = SERIAL_VALUES["message-start"]
+    MAGIC_VALUE = int(SERIAL_VALUES["magic-value"])
+    MESSAGE_START = int("0x" + SERIAL_VALUES["message-start"], 0)
 
 
 def byte_len(data: str) -> int:
     return int(len(data)/4)
 
 
-def calculate_security_sum(byte_str: str) -> str:
+def calculate_security_sum(data: bytearray) -> int:
     value = MAGIC_VALUE
-    for i in range (0, byte_len(byte_str)):
-        start = 4*i+2
-        end = 4*i+4
-        value += int("0x" + byte_str[start:end], 0)
+    for d in data:
+        value += int(d)
 
-    return tohex(value % 256)
+    return value % 256
 
 
-def prepare_serial_message(device_id:str, serial_data: str) -> str:
-    message = MESSAGE_START
-    message += tohex(byte_len(serial_data))
-    message += device_id
-    message += serial_data
-    message += calculate_security_sum(message)
-    return message
+def prepare_serial_message(device_id:str, serial_data: bytearray) -> bytes:
+    message = bytearray()
+    message.append(MESSAGE_START)
+    message.append(len(serial_data))
+    message.append(int("0x" + device_id[:2], 0))
+    message.append(int("0x" + device_id[2:], 0))
+    message.extend(serial_data)
+    message.append(calculate_security_sum(message))
+    return bytes(message)
 
 
-def send_serial_packet(device_id: str, serial_data: str) -> None:
+def send_serial_packet(device_id: str, serial_data: bytearray) -> None:
     message = prepare_serial_message(device_id, serial_data)
     print(f"Sending: {message}")
 
     # TODO: device search
-    # with open("/dev/ttyACM0") as tty:
-    #     for i in range(0, RETRY_COUNT):
-    #         tty.write(message)
-    #     tty.close()
+    with open("/dev/ttyACM0", "wb") as tty:
+        for i in range(0, RETRY_COUNT):
+            tty.write(message)
+            print(f"Sending try #{i+1}")
+        tty.close()
 
-def tohex(value: int, length=1) -> str:
-    string = hex(value)[2:]
-    ret = ""
-
-    while len(string) < length*2:
-        string = "0" + string
-
-    for i in range(0, length):
-        ret += f"\\x{string[i*2:i*2+2]}"
-
-    return ret
 
 def set_setting(typ: str, name: str, value: int, device_id: str):
-    length = SERIAL_VALUES[typ][name]["length"]
-    id = tohex(SERIAL_VALUES[typ][name]["id"])
+    length = int(SERIAL_VALUES[typ][name]["length"])
+    setting_id = int(SERIAL_VALUES[typ][name]["id"])
 
-    data = tohex(value, length-1)
-    data = id + data
+    data = bytearray()
+    data.append(setting_id)
+    data.extend(value.to_bytes(length-1))
+
     send_serial_packet(device_id, data)
+
 
 # helper functions
 def set_base_setting(name: str, value: int) -> None:
