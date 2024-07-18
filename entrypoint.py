efcf11b1a77d9d30f83e174ee777004c852b2e36
Tomasz Pakuła <forest10pl@gmail.com>
2024-07-18 18:51:53 +0200

    App is now resizable

--- entrypoint.py
+++ entrypoint.py
@@ -7,7 +7,6 @@ import argparse
 parser = argparse.ArgumentParser("boxflat")
 parser.add_argument("--local", help="Run boxflat from repository folder", action="store_true", required=False)
 parser.add_argument("--dry-run", help="Don't send any data to the serial devices", action="store_true", required=False)
-parser.add_argument("--resizable", help="Make main window resizable", action="store_true", required=False)
 parser.add_argument("--data-path", help="Use arbitrary data path", type=str, required=False)
 args = parser.parse_args()
 
@@ -18,5 +17,5 @@ if args.data_path:
 if args.local:
     data_path = "data"
 
-app = app.MyApp(data_path, args.dry_run, args.resizable, application_id="com.lawstorant.boxflat")
+app = app.MyApp(data_path, args.dry_run, application_id="com.lawstorant.boxflat")
 app.run()
