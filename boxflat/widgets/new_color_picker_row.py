9539a65470eba762bff1556a6b4067f73804470f
Tomasz Pakuła <forest10pl@gmail.com>
2024-08-15 18:54:52 +0200

    Fix possible RPM color mangling
    A lack of cooldown and a bug in reading RPM color values could lead
    to RPM colors reading button colors and immadietely setting them.
    New color picker lacked the use of mute and cooldown

--- boxflat/widgets/new_color_picker_row.py
+++ boxflat/widgets/new_color_picker_row.py
@@ -47,17 +47,26 @@ class BoxflatNewColorPickerRow(BoxflatRow):
 
 
     def set_led_value(self, value: list, index: int) -> None:
-        if index in range(len(self._colors)):
-            rgba = Gdk.RGBA()
-            rgba.parse(f"rgb({value[0]},{value[1]},{value[2]})")
+        if index not in range(len(self._colors)):
+            return
 
-            self._colors[index].set_rgba(rgba)
+        if self.cooldown():
+            print("Still cooling down")
+            return
+
+        self._mute = True
+        rgba = Gdk.RGBA()
+        rgba.parse(f"rgb({value[0]},{value[1]},{value[2]})")
+
+        self._colors[index].set_rgba(rgba)
+        self._mute = False
 
 
     def _notify(self, button: Gtk.ColorDialogButton, param) -> None:
         if self._mute:
             return
 
+        self._cooldown = 1
         index = self._colors.index(button)
         for sub in self._subscribers:
             sub[0](self.get_value(index), sub[2][0] + str(index+1))
