3954f3645577962321dfef0b97bc3a4c4016ed46
Tomasz Pakuła <forest10pl@gmail.com>
2024-09-06 13:52:09 +0200

    Fix wheel settings detection

--- boxflat/panels/wheel.py
+++ boxflat/panels/wheel.py
@@ -73,27 +73,27 @@ class WheelSettings(SettingsPanel):
 
         self._current_row.subscribe(self._cm.set_setting_int, "wheel-paddles-mode")
         self._append_sub("wheel-paddles-mode", self._current_row.set_value)
-        self._append_sub("wheel-paddles-mode", self._current_row.set_present)
+        self._append_sub_connected("wheel-paddles-mode", self._current_row.set_present)
         self._current_row.set_present(False)
 
 
         level = BoxflatLevelRow("Combined Paddles", max_value=65534)
         self._add_row(level)
         self._append_sub_hid(MozaAxis.COMBINED_PADDLES, self._current_row.set_value)
-        self._append_sub("wheel-paddles-mode", lambda v: level.set_present(v == 2))
+        self._append_sub_connected("wheel-paddles-mode", lambda v: level.set_present(v == 2))
         self._append_sub("wheel-clutch-point", self._current_row.set_offset)
         paddle_mode.subscribe(lambda v: level.set_present(v == 2, skip_cooldown=True))
         self._current_row.set_present(0, skip_cooldown=True, trigger_cooldown=False)
 
         self._add_row(BoxflatLevelRow("Left Paddle", max_value=65534))
         self._append_sub_hid(MozaAxis.LEFT_PADDLE, self._current_row.set_value)
-        self._append_sub("wheel-paddles-mode", self._current_row.set_present, -2)
+        self._append_sub_connected("wheel-paddles-mode", self._current_row.set_present, -2)
         paddle_mode.subscribe(self._current_row.set_present, -2, True)
         self._current_row.set_present(0, skip_cooldown=True, trigger_cooldown=False)
 
         self._add_row(BoxflatLevelRow("Right Paddle", max_value=65534))
         self._append_sub_hid(MozaAxis.RIGHT_PADDLE, self._current_row.set_value)
-        self._append_sub("wheel-paddles-mode", self._current_row.set_present, -2)
+        self._append_sub_connected("wheel-paddles-mode", self._current_row.set_present, -2)
         paddle_mode.subscribe(self._current_row.set_present, -2, True)
         self._current_row.set_present(0, skip_cooldown=True, trigger_cooldown=False)
 
@@ -107,8 +107,8 @@ class WheelSettings(SettingsPanel):
         self._current_row.subscribe(level.set_offset)
         paddle_mode.subscribe(lambda v: slider.set_active(v == 2))
         self._append_sub("wheel-clutch-point", self._current_row.set_value)
-        self._append_sub("wheel-clutch-point", self._current_row.set_present)
         self._append_sub("wheel-paddles-mode", lambda v: slider.set_active(v == 2))
+        self._append_sub_connected("wheel-clutch-point", self._current_row.set_present)
         self._current_row.set_present(False)
 
         self._add_row(BoxflatToggleButtonRow("Rotary Encoder Mode"))
@@ -130,7 +130,7 @@ class WheelSettings(SettingsPanel):
         self._current_row.add_marks(5, 10)
         self._current_row.subscribe(self._cm.set_setting_int, "wheel-buttons-brightness")
         self._append_sub("wheel-buttons-brightness", self._current_row.set_value)
-        self._append_sub_connected("wheel-buttons-brightness", self._current_row.set_active, +1)
+        self._append_sub_connected("wheel-buttons-brightness", self._current_row.set_present, +1)
 
         self._add_row(BoxflatSliderRow("RPM Brightness", range_end=15))
         self._current_row.add_marks(5, 10)
