e129d1898b9c321df0cb95e4eafdf9c255300b7e
Tomasz Pakuła <forest10pl@gmail.com>
2025-01-18 00:06:15 +0100

    Update naming

--- boxflat/panels/sequential.py
+++ boxflat/panels/sequential.py
@@ -17,7 +17,7 @@ class SequentialSettings(SettingsPanel):
 
 
     def prepare_ui(self):
-        self.add_preferences_group("Shifter Settings")
+        self.add_preferences_group("Shift Settings")
         self._add_row(BoxflatSwitchRow("Reverse Shift Direction"))
         self._current_row.subscribe(self._cm.set_setting, "sequential-direction")
         self._cm.subscribe("sequential-direction", self._current_row.set_value)
@@ -28,8 +28,8 @@ class SequentialSettings(SettingsPanel):
         self._current_row.subscribe(self._cm.set_setting, "sequential-paddle-sync")
         self._cm.subscribe("sequential-paddle-sync", self._current_row.set_value)
 
-        self.add_preferences_group("Buttons")
-        self._add_row(BoxflatSliderRow("Button Brightness", 0, 10))
+        self.add_preferences_group("Lights")
+        self._add_row(BoxflatSliderRow("Buttons Brightness", 0, 10))
         self._current_row.add_marks(5)
         self._current_row.set_slider_width(294)
         self._current_row.subscribe(self._cm.set_setting, "sequential-brightness")
--- boxflat/panels/stalks.py
+++ boxflat/panels/stalks.py
@@ -25,22 +25,22 @@ class StalksSettings(SettingsPanel):
 
 
     def prepare_ui(self):
-        self.add_preferences_group("Stalks settings")
+        self.add_preferences_group("Compatibility modes")
 
-        self._add_row(BoxflatSwitchRow("Turn Signal Compatibility Mode", "Use turn signals with games that lack cancel binding"))
-        self._current_row.subscribe(self._settings.write_setting, "stalks-signal-compatibility")
+        self._add_row(BoxflatSwitchRow("Turn Signals", "Press button again when cancelling"))
+        self._current_row.subscribe(self._settings.write_setting, "stalks-turnsignal-compat")
         self._current_row.subscribe(self._hid_handler.stalks_turnsignal_compat_active)
-        self._current_row.set_value(self._settings.read_setting("stalks-signal-compatibility") or 0, mute=False)
+        self._current_row.set_value(self._settings.read_setting("stalks-turnsignal-compat") or 0, mute=False)
 
-        self._add_row(BoxflatSwitchRow("Headlights Compatibility Mode", "Use your stalks with games that lack Cancel binding"))
-        self._current_row.subscribe(self._settings.write_setting, "stalks-headlights-compatibility")
+        self._add_row(BoxflatSwitchRow("Headlights (WIP)", "Cycling instead of discrete buttons"))
+        self._current_row.subscribe(self._settings.write_setting, "stalks-headlights-compat")
         self._current_row.subscribe(self._hid_handler.stalks_turnsignal_compat_active)
-        self._current_row.set_value(self._settings.read_setting("stalks-headlights-compatibility") or 0, mute=False)
+        self._current_row.set_value(self._settings.read_setting("stalks-headlights-compat") or 0, mute=False)
         self._current_row.set_active(0)
 
-        self._add_row(BoxflatSwitchRow("Wipers Compatibility Mode", "Use your stalks with games that lack Cancel binding"))
-        self._current_row.subscribe(self._settings.write_setting, "stalks-wipers-compatibility")
+        self._add_row(BoxflatSwitchRow("Wipers (WIP)", "Up/Down instead of discrete buttons"))
+        self._current_row.subscribe(self._settings.write_setting, "stalks-wipers-compat")
         self._current_row.subscribe(self._hid_handler.stalks_turnsignal_compat_active)
-        self._current_row.set_value(self._settings.read_setting("stalks-wipers-compatibility") or 0, mute=False)
+        self._current_row.set_value(self._settings.read_setting("stalks-wipers-compat") or 0, mute=False)
         self._current_row.set_active(0)
 
