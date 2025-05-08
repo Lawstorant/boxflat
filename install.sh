f43f18c7fc4cf1e63264173773dc992655e0d712
Tomasz Pakuła <forest10pl@gmail.com>
2025-05-08 12:18:31 +0200

    Change udev rules installation path

--- install.sh
+++ install.sh
@@ -13,12 +13,12 @@ if [[ $1 == "remove" || $3 == "remove" ]]; then
     rm "$PREFIX/usr/share/metainfo/"*boxflat*
     rm -rf "$PREFIX/usr/share/boxflat"
     cp -r ./icons/* "$PREFIX/usr/share/icons/hicolor/"
-    rm "$PREFIX/etc/udev/rules.d/"*boxflat*.rules
+    rm "$PREFIX/usr/lib/udev/rules.d/"*boxflat*.rules
     exit 0
 fi
 
 if [[ -n $PREFIX ]]; then
-    mkdir -p "$PREFIX/etc/udev/rules.d"
+    mkdir -p "$PREFIX/usr/lib/udev/rules.d"
     mkdir -p "$PREFIX/usr/bin"
     mkdir -p "$PREFIX/usr/share/applications"
 fi
@@ -36,7 +36,7 @@ cp ./*metainfo.xml "$PREFIX/usr/share/metainfo/"
 
 cp --preserve=mode "boxflat.sh" "$PREFIX/usr/bin/boxflat"
 cp ./*.desktop "$PREFIX/usr/share/applications/"
-cp udev/* "$PREFIX/etc/udev/rules.d/"
+cp udev/* "$PREFIX/usr/lib/udev/rules.d/"
 
 # refresh udev so the rules take effect immadietely
 if [[ $1 == "no-udev" || $3 == "no-udev" ]]; then
