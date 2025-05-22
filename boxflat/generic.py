f9168d726208d022a76d5cf336689bb21676226f
Tomasz Pakuła <forest10pl@gmail.com>
2025-05-22 16:39:00 +0200

    Generic devices improvements (#87)

--- boxflat/generic.py
+++ boxflat/generic.py
@@ -7,6 +7,8 @@ from evdev.device import AbsInfo
 from threading import Thread, Event
 from boxflat.subscription import SimpleEventDispatcher
 
+GAMEPAD_RANGE = range(BTN_GAMEPAD, BTN_THUMBR+1)
+
 class GenericDevice(SimpleEventDispatcher):
     def __init__(self, entry: dict):
         super().__init__()
@@ -14,12 +16,28 @@ class GenericDevice(SimpleEventDispatcher):
         self._name = entry["name"]
         self._shutdown = Event()
         self._device = None
+        self._ignore_buttons = False
+        self._ignore_axes = False
+
+        if "ignore-buttons" in entry:
+            self._ignore_buttons = entry["ignore-buttons"]
+
+        if "ignore-axes" in entry:
+            self._ignore_axes = entry["ignore-axes"]
 
         Thread(daemon=True, target=self._hid_read_loop).start()
 
 
     def shutdown(self):
         self._shutdown.set()
+
+
+    def change_ignore_setting(self, type: str, state: bool):
+        if type == "buttons":
+            self._ignore_buttons = state
+
+        if type == "axes":
+            self._ignore_axes = state
 
 
     def _try_open(self) -> evdev.InputDevice:
@@ -66,6 +84,15 @@ class GenericDevice(SimpleEventDispatcher):
 
             try:
                 for event in self._device.read_loop():
+                    if event.type == EV_KEY and self._ignore_buttons:
+                        continue
+
+                    if event.type == EV_ABS and self._ignore_axes:
+                        continue
+
+                    if event.type == EV_KEY and event.code in GAMEPAD_RANGE:
+                        event.code -= 16
+
                     new_device.write_event(event)
                     if self._shutdown.is_set():
                         break
@@ -101,13 +128,38 @@ class GenericDevice(SimpleEventDispatcher):
 
         # Add necessary event types
         if EV_ABS not in cap:
-            cap[EV_ABS] = [(ABS_RZ, AbsInfo(0, 0, 255, 8 ,8, 0))]
+            cap[EV_ABS] = []
+
+        x = False
+        y = False
+
+        for axis in cap[EV_ABS]:
+            if axis[0] == ABS_X:
+                x = True
+
+            if axis[0] == ABS_Y:
+                y = True
+
+        if not x:
+            cap[EV_ABS].append((ABS_X, AbsInfo(0, 0, 255, 8 ,8, 0)))
+
+        if not y:
+            cap[EV_ABS].append((ABS_Y, AbsInfo(0, 0, 255, 8 ,8, 0)))
 
         if EV_KEY not in cap:
             cap[EV_KEY] = [BTN_JOYSTICK]
 
         elif BTN_JOYSTICK not in cap[EV_KEY]:
-            cap[EV_KEY].append(BTN_JOYSTICK)
+            for btn in GAMEPAD_RANGE:
+                if btn not in cap[EV_KEY]:
+                    continue
+
+                # translate to joystick range
+                cap[EV_KEY].remove(btn)
+                cap[EV_KEY].append(btn - 16)
+
+            if BTN_JOYSTICK not in cap[EV_KEY]:
+                cap[EV_KEY].append(BTN_JOYSTICK)
 
         # Create new device
         new_device = evdev.UInput(cap, vendor=device.info.vendor, product=device.info.product, name=device.name)
