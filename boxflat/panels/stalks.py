aa3bc6894bd6def9bf15bb10eca6b2110b27eef6
Tomasz Pakuła <forest10pl@gmail.com>
2025-07-07 22:16:01 +0200

    Make it possible to skip "positional lights" stage

--- boxflat/panels/stalks.py
+++ boxflat/panels/stalks.py
@@ -16,6 +16,7 @@ class StalksSettings(SettingsPanel):
         self._wipers       = None
         self._wpiers_alt   = None
         self._wipers_quick = None
+        self._skip         = None
 
         self._settings = settings
         super().__init__("Multifunction Stalks", button_callback, connection_manager, hid_handler)
@@ -48,11 +49,23 @@ class StalksSettings(SettingsPanel):
         self._turn_hold = self._current_row
 
         self.add_preferences_group()
+        compat = self._settings.read_setting("stalks-headlights-compat") or 0
         self._add_row(BoxflatSwitchRow("Headlights", "Cycling instead of discrete buttons"))
         self._current_row.subscribe(self._settings.write_setting, "stalks-headlights-compat")
         self._current_row.subscribe(self._hid_handler.stalks_headlights_compat_active)
-        self._current_row.set_value(self._settings.read_setting("stalks-headlights-compat") or 0, mute=False)
+        self._current_row.set_value(compat, mute=False)
         self._headlights = self._current_row
+
+        positional = self._settings.read_setting("stalks-skip-positional") or 0
+        self._add_row(BoxflatSwitchRow("Skip positional", "For games that only have on/off headlights"))
+        self._current_row.set_active(compat)
+        self._current_row.set_value(positional)
+        self._current_row.subscribe(self._settings.write_setting, "stalks-skip-positional")
+        self._current_row.subscribe(self._hid_handler.stalks_headlights_skip_pos_active)
+        self._skip = self._current_row
+        self._headlights.subscribe(self._skip.set_active)
+        self._hid_handler.stalks_headlights_skip_pos_active(positional)
+
 
         self._wipers1 = BoxflatSwitchRow("Wipers", "Cycling instead of discrete buttons")
         self._wipers2 = BoxflatSwitchRow("Wipers alternative", "Up/Down instead of discrete buttons")
@@ -74,13 +87,15 @@ class StalksSettings(SettingsPanel):
         self._current_row.set_value(self._settings.read_setting("stalks-wipers-compat2") or 0, mute=False)
         self._wpiers_alt = self._current_row
 
+        quick = self._settings.read_setting("stalks-wipers-quick") or 0
         self._quick_wipe = BoxflatSwitchRow("Quick wipe emulation", "One and done")
         self._add_row(self._quick_wipe)
         self._current_row.set_active(0)
         self._current_row.subscribe(self._settings.write_setting, "stalks-wipers-quick")
         self._current_row.subscribe(self._hid_handler.stalks_wipers_quick_active)
-        self._current_row.set_value(self._settings.read_setting("stalks-wipers-quick") or 0, mute=False)
+        self._current_row.set_value(quick)
         self._wipers_quick = self._current_row
+        self._hid_handler.stalks_wipers_quick_active(quick)
 
         self._turn_toggle.disable_cooldown()
         self._turn_hold.disable_cooldown()
@@ -110,6 +125,7 @@ class StalksSettings(SettingsPanel):
             "turn-signal-toggle" : self._turn_toggle.get_value(),
             "turn-signal-hold"   : self._turn_hold.get_value(),
             "headlights"         : self._headlights.get_value(),
+            "skip-positional"    : self._skip.get_value(),
             "wipers"             : self._wipers.get_value(),
             "wipers-alt"         : self._wpiers_alt.get_value(),
             "wipers-quick"       : self._wipers_quick.get_value()
@@ -123,6 +139,9 @@ class StalksSettings(SettingsPanel):
         self._wipers.set_value(settings["wipers"], mute=False)
         self._wpiers_alt.set_value(settings["wipers-alt"], mute=False)
         self._wipers_quick.set_value(settings["wipers-quick"], mute=False)
+
+        if "skip-positional" in settings:
+            self._skip.set_value(settings["skip-positional"], mute=False)
 
 
     def reset(self, *_) -> None:
